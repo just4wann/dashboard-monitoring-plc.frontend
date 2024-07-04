f62b5ba1a075966e8fb9824d798a5392cab505f1
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-07-05 00:26:19 +0700

    Relayouting Sidebar section

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,23 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="flex flex-col justify-start items-start bg-slate-800 w-80 h-screen py-4 px-6">
-    <h1 class="font-semibold text-xl mx-auto">PLC<span class="text-red-600">Dash</span></h1>
-    <ul>
-      <li>
-        <NuxtLink to="/dashboard">Dashboard</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/chart">Chart</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/info">Line Information</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/operator">Operator</NuxtLink>
-      </li>
-    </ul>
-  </main>
+  <UCard class="w-80 h-screen">
+    <main class="flex flex-col justify-start items-start gap-10 w-full py-4 px-6">
+      <h1 class="font-semibold text-2xl text-center">PLC<span class="text-red-600">Dash</span></h1>
+      <ul class="flex flex-col gap-5 justify-center items-start w-full font-semibold tracking-wider">
+        <NuxtLink to="/dashboard">
+          <li class="flex justify-center items-center gap-3">
+            <UIcon name="i-heroicons-squares-2x2-16-solid" class="text-xl" />
+            <p>Dashboard</p>
+            <UKbd>D</UKbd>
+          </li>
+        </NuxtLink>
+        <NuxtLink to="/info">
+          <li class="flex justify-center items-center gap-3">
+            <UIcon name="i-heroicons-exclamation-circle-16-solid" class="text-xl" />
+            <p>Line Info</p>
+            <UKbd>L</UKbd>
+          </li>
+        </NuxtLink>
+        <NuxtLink to="/chart">
+          <li class="flex justify-center items-center gap-3">
+            <UIcon name="i-heroicons-chart-pie-16-solid" class="text-xl" />
+            <p>Charts</p>
+            <UKbd>C</UKbd>
+          </li>
+        </NuxtLink>
+      </ul>
+    </main>
+  </UCard>
 </template>
 
 <style scoped></style>
