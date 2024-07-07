dd27ac4375cf3c357977ba5b92cb804ccbb6e0b1
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-07-07 18:15:32 +0700

    add app.config.ts for ui config

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,11 @@ export default defineAppConfig({
         card: {
             background: 'bg-white dark:bg-[#171717]',
             ring: 'ring-1 ring-gray-200 dark:ring-[#252525]'
+        },
+        alert: {
+            title: 'text-sm font-medium',
+            description: 'mt-1 text-xs leading-4 opacity-90 tracking-wider',
+            padding: 'px-4 py-3'
         }
     }
 })
