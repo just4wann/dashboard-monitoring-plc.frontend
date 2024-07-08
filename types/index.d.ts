ae3047f27034fd2c1c7a8cf63cd8ba5d4d9fb0f6
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-07-08 21:42:38 +0700

    add new type for date format

--- types/index.d.ts
+++ types/index.d.ts
@@ -13,3 +13,13 @@ export type PieChartDataConfig = {
     label: string;
     value: number;
 } 
+
+export type Period = 'daily' | 'weekly' | 'monthly';
+export interface Range {
+    start: Date;
+    end: Date;
+}
+export type DataRecord = {
+    date: Date;
+    value: number;
+}
