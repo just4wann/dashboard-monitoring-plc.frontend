a427c0dabc166efa1cd1f15d74e2247252c85ba4
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2024-08-11 21:23:34 +0700

    add composable method for using Websocket

---
+++ composables/useWebSockets.ts
@@ -0,0 +1,40 @@
+import { io, Socket } from 'socket.io-client'
+import { useStore } from '~/store'
+import type { EfficiencyDataRecord, TemperatureDataRecord, PressureDataRecord, ProductionInfoDataRecord, MachineInfoDataRecord, TroubleMachineDataRecord } from '~/types'
+
+export const useWebSockets = (): Socket => {
+    const socket = io('http://localhost:80');
+    socket.on('connect', () => {
+        if (socket.connected) {
+            console.log('Connected to WebSocket')
+        }
+        if (socket.disconnected) {
+            console.log('Disconnected from WebSocket')
+        }
+    });
+    socket.on('efficiencies', (value: EfficiencyDataRecord) => {
+        const store = useStore();
+        store.dataEfficiency.push(value);
+    });
+    socket.on('temperatures', (value: TemperatureDataRecord) => {
+        const store = useStore();
+        store.dataTemperature.push(value);
+    });
+    socket.on('pressures', (value: PressureDataRecord) => {
+        const store = useStore();
+        store.dataPressure.push(value);
+    });
+    socket.on('productions', (value: ProductionInfoDataRecord) => {
+        const store = useStore();
+        store.dataProduction.unshift(value);
+    })
+    socket.on('machines', (value: MachineInfoDataRecord) => {
+        const store = useStore();
+        store.dataMachine.unshift(value);
+    })
+    socket.on('troubles', (value: TroubleMachineDataRecord) => {
+        const store = useStore();
+        store.dataTrouble.unshift(value);
+    })
+    return socket;
+}
